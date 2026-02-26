a87df82be1b0c75a9844237056d63340a954f368
Rose Lapers <22501222@me.uvsq.fr>
2026-02-26 11:26:40 +0100

    ajout de l'operation multiplication

--- pycalc.py
+++ pycalc.py
@@ -5,6 +5,8 @@ def calc(nb1, nb2, ope) :
         return nb1+nb2
     elif ope == "-" :
         return nb1-nb2
+    elif ope == "*" :
+        return nb1*nb2
     return None
 
 calc(float(sys.agrv[1]), float(sys.argv[3]), sys.argv[2])
