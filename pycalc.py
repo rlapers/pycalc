b745b46381c39b0ba4467c75d343bf2b9726301a
Rose Lapers <22501222@me.uvsq.fr>
2026-02-26 11:25:41 +0100

    ajout de l'operation soustraction

--- pycalc.py
+++ pycalc.py
@@ -3,6 +3,8 @@ import sys
 def calc(nb1, nb2, ope) :
     if ope == "+" :
         return nb1+nb2
+    elif ope == "-" :
+        return nb1-nb2
     return None
 
 calc(float(sys.agrv[1]), float(sys.argv[3]), sys.argv[2])
