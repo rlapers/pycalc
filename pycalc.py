70f0fa1b1a694ddfe09000a49749c909f53e7344
Rose Lapers <22501222@me.uvsq.fr>
2026-02-26 11:43:13 +0100

    multiplication differente

--- pycalc.py
+++ pycalc.py
@@ -6,7 +6,11 @@ def calc(nb1, nb2, ope) :
     elif ope == "-" :
         return nb1-nb2
     elif ope == "*" :
-        return nb1*nb2
+        cmp = 0
+        res = 0
+        while cmp < nb2 :
+            res += nb1
+            cmp += 1
     return None
 
-calc(float(sys.agrv[1]), float(sys.argv[3]), sys.argv[2])
+calc(float(sys.argv[1]), float(sys.argv[3]), sys.argv[2])
